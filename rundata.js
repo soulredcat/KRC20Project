5dcfe2600ea412c2f1458beac26c84fc549def94
REDcat <arifsofyankusumadiarto@gmail.com>
2024-10-24 20:16:47 +0700

    decimal

--- rundata.js
+++ rundata.js
@@ -2,9 +2,9 @@ const { createDatabase, createTables } = require('./dataentry/dblogin/krcinfodbn
 const { fetchDataAndSave } = require('./dataentry/data/kasdata');
 
 async function main() {
-    await createDatabase();    // Create the database
-    await createTables();      // Create the tables
-    await fetchDataAndSave();  // Fetch and store the data
+    await createDatabase(); 
+    await createTables();  
+    await fetchDataAndSave(); 
 }
 
-setInterval(main, 30000);  // Run every 30 seconds
+setInterval(main, 30000);
